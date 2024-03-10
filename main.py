df66b65d857f25f66786afd4f5f2cf4407d280d1
PEEMTM <peem33302@gmail.com>
2024-03-10 15:22:58 +0700

    update

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def main():
 
     # Load a model
     #model = YOLO("yolov8n.yaml")  # build a new model from scratch
-    model = YOLO(r"runs\detect\train4\weights\best.pt")  # load a pretrained model (recommended for training)
+    model = YOLO(r"runs\detect\train5\weights\best.pt")  # load a pretrained model (recommended for training)
     model.to(device)
 
     # Use the model
-    results = model.train(data=r"E:\dataset\ObjectDetectionFinal.v1i.yolov8\data.yaml", epochs=10, imgsz=640)  # train the model
+    results = model.train(data=r"D:\TaDee\Dataset\TaDee Dectection.v2i.yolov8\data.yaml", epochs=100, imgsz=640)  # train the model
 
     # Remember to deactivate your virtual environment when you're done:
     # deactivate  # On Windows
